df84be7c5994e75f27598dab3e140e517bc8b2ec
andriyko66 <147740137+andriyko66@users.noreply.github.com>
2025-12-18 00:46:52 +0200

    Create MainActivity.kt

---
+++ MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.lab14
+
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodChannel
+
+class MainActivity : FlutterActivity() {
+
+    private val CHANNEL = "native_channel"
+
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+
+        MethodChannel(
+            flutterEngine.dartExecutor.binaryMessenger,
+            CHANNEL
+        ).setMethodCallHandler { call, result ->
+            when (call.method) {
+                "getStaticText" -> result.success("Hi, Mom!")
+                else -> result.notImplemented()
+            }
+        }
+    }
+}
